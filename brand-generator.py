507304563147e67f4f0e5207467026bc017e90c6
shree1602 <89871187+shree1602@users.noreply.github.com>
2021-10-06 12:56:27 +0530

    Create brand-generator.py
    This is Brand Generator Program. Which is the first day program/exercise in Angela's course.

---
+++ brand-generator.py
@@ -0,0 +1,12 @@
+# This is the Brand Generator Program
+#For Any questions/comments: Please reach me
+#You can reach me at shreenivas.sk@gmail.com 
+# My WhatsApp no is +918668150603
+
+print("Welcome to Brand Generator\n")
+
+city = input("Which City You are born? \n")
+
+pet = input("What is your first pet name? \n")
+
+print("Your brand name can be",city+" "+pet)
